8a4ffb3c4557352070ee6b6d5f63ab514262c4e9
Любовь <lubovpeksa34008@gmail.com>
2018-05-04 20:40:54 +0300

    тут все довольно просто

---
+++ scrapers/lenta/lenta/items.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class LentaItem(scrapy.Item):
+    Title = scrapy.Field()
+    Date = scrapy.Field()
+    Text = scrapy.Field()
+    url = scrapy.Field()
---
+++ scrapers/lenta/lenta/spiders/lenta_scrape.py
@@ -0,0 +1,36 @@
+import scrapy
+from lenta.items import LentaItem
+from datetime import datetime
+import re
+import time
+
+
+class Lenta(scrapy.Spider):
+    name = 'lenta_scraper'
+    start_urls = []
+    #стартовая страница, скачиваются все данные с заданного дня до этого момента
+    start_urls.append("https://lenta.ru/news/2018/04/21")
+
+    def parse(self, response):
+        for href in response.xpath("//div[contains(@class, 'titles')]//a//@href"):
+            # add the scheme, eg http://
+            url  = response.urljoin(href.extract())
+            yield scrapy.Request(url, callback=self.parse_dir_contents)
+
+        list_pages = response.xpath("//div[contains(@class, 'b-archive__header-date')]//a//@href").extract()
+        if len(list_pages) == 3: # на последней странице нет кнопки next, поэтому кнопок меньше
+            next_page = response.urljoin(list_pages[-1])
+            yield scrapy.Request(next_page, callback=self.parse)
+
+    def parse_dir_contents(self, response):
+        item = LentaItem()
+
+        item['Title'] = response.xpath("//h1[@class= 'b-topic__title']//descendant::text()").extract_first()
+
+        item['Date']= response.xpath("//div[@class= 'b-topic__info']//@datetime").extract_first()
+
+        item['Text']  = " ".join(response.xpath("//div[contains(@class, 'b-text clearfix js-topic__text')]").xpath(".//p/descendant::text()").extract()).strip()
+
+        item['url'] = response.xpath("//meta[@property='og:url']/@content").extract()[0]
+
+        yield item
